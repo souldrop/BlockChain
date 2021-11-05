0670a6fec6db2aaecb1e290bcdc09802bc2da30a
souldrop <imneocoder@gmail.com>
2021-11-05 07:59:12 +0530

    updatedcontract --sol version

---
+++ inbox-updated/deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+
+const { abi, evm } = require('./compile');
+
+provider = new HDWalletProvider(
+  'same amateur left vast crater barely assist kick language peanut aware jungle',
+  'https://rinkeby.infura.io/v3/a3f69fde9eb14c008fbb270f1d5c1b1a'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+
+deploy();
